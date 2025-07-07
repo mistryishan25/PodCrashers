8c245ab7cbef0ce1a74bd62196ea82a8ca601d0d
ihmistry <ihmistry@ncsu.edu>
2025-07-07 19:12:11 -0400

    skeletion files added for database operations

---
+++ CRUD_operaions.py
@@ -0,0 +1,27 @@
+def create_episode(episode_data):
+    # Code to create a new episode in the database
+    pass
+
+def read_episode(episode_uuid):
+    # Code to read an existing episode
+    pass
+
+def update_episode(episode_uuid, updated_data):
+    # Code to update an existing episode
+    pass
+
+def delete_episode(episode_uuid):
+    # Code to delete an existing episode
+    # This is needed to free up resources, so that new podcasts or episodes can be added
+    # we can ask user to confirm deletion
+    pass
+
+def find_episodes(podcast_id):
+    # Code to find an episode by name
+    pass
+
+def get_podcast_url(episode_id):
+    # Code to get the URL of a podcast episode
+
+
+def 
